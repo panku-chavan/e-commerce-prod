be64bfe0a3bb59dc81c391a8079e55a6e8c62871
panku-chavan <pankajchavan963@gmail.com>
2023-05-18 18:21:46 +0530

    footer page update

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,32 +1,73 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BsGithub, BsInstagram } from "react-icons/bs";
-import { AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
+
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagramSquare,
+  FaLinkedinIn,
+  FaGithubSquare,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div>
-        <h3 className="text-center">All Right Reserved &copy; Pankaj Chavan</h3>
-        <p className="text-center mt-3">
-          <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
-          <Link to="/policy">Privacy Policy</Link>
-        </p>
-      </div>
-      <div className="text-center">
-        <a href="https://github.com/panku-chavan" target="blank">
-          <BsGithub />
-        </a>
-        <a href="https://www.linkedin.com/in/pankaj-chavan-9ba6681ba/">
-          <AiFillLinkedin />
-        </a>
-        <a href="https://www.instagram.com/panku_chavan/">
-          <BsInstagram />
-        </a>
-        <a href="https://twitter.com/panku_chavan">
-          <AiFillTwitterCircle />
-        </a>
-      </div>
+    <div className="" style={{ minHeight: "" }}>
+      <footer className="bg-dark text-center text-white">
+        <div className="container">
+          <section className="">
+            <Link
+              className="btn btn-outline-light btn-floating m-2 mt-4 mb-4"
+              to={"https://www.facebook.com/pankajsubhashchavan/"}
+              role="button"
+            >
+              <FaFacebookSquare />
+            </Link>
+
+            <Link
+              className="btn btn-outline-light btn-floating m-2 mt-4 mb-4"
+              to={"https://twitter.com/panku_chavan"}
+              role="button"
+            >
+              <FaTwitterSquare />
+            </Link>
+
+            <Link
+              className="btn btn-outline-light btn-floating m-2 mt-4 mb-4"
+              to={"https://www.instagram.com/panku_chavan/"}
+              role="button"
+            >
+              <FaInstagramSquare />
+            </Link>
+
+            <Link
+              className="btn btn-outline-light btn-floating m-2 mt-4 mb-4"
+              to={"https://www.linkedin.com/in/pankaj-chavan-9ba6681ba/"}
+              role="button"
+            >
+              <FaLinkedinIn />
+            </Link>
+
+            <Link
+              className="btn btn-outline-light btn-floating m-2 mt-4 mb-4"
+              to={"https://github.com/panku-chavan/"}
+              role="button"
+            >
+              <FaGithubSquare />
+            </Link>
+          </section>
+        </div>
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2023 Copyright :
+          <span className="text-white" to={""}>
+            {" "}
+            panku-chavan
+          </span>
+        </div>
+        {/* Copyright */}
+      </footer>
     </div>
   );
 };
